ef07f12ef399634e409ab365f87e2fff3db136a2
greatmomo <66915435+greatmomo@users.noreply.github.com>
2021-10-23 17:36:56 +0200

    Add rough draft of list filling
    Can now dynamically fill a dropdown list

--- public/main.js
+++ public/main.js
@@ -1,3 +1,12 @@
 function myFunction() {
     document.getElementById("encounterPrint").innerHTML="Generation started...";
+
+    var selectElem = document.getElementById("pickone");
+    var items;
+    items = ["Abyss", "Mountains", "Plains", "Random"];
+    for (var item of items) {
+        var element = document.createElement("option");
+        element.innerText = item;
+        selectElem.append(element);
+    }
 }
