88744be6a27c8a615e97ee51236c6d52737c26df
greatmomo <66915435+greatmomo@users.noreply.github.com>
2021-10-25 18:42:55 +0200

    Add express stuff

--- public/index.js
+++ public/index.js
@@ -1,4 +1,14 @@
+const express = require('express')
+const app = express();
+const port = 8000;
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
 
 function myFunction() {
     document.getElementById("encounterPrint").innerHTML="Generation started...";
